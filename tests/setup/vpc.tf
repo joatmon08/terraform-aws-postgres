d370d737b2d2e2efa16848a7ae20d0dbac271003
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-27 18:33:33 -0400

    Update tests with db secrets engine

--- tests/setup/vpc.tf
+++ tests/setup/vpc.tf
@@ -68,7 +68,7 @@ resource "aws_security_group" "database" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = [var.hcp_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
